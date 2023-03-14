05b7eb022bb7e36688b986430d557c4698155596
Raymond Konarski <konarskiraymond@gmail.com>
2023-03-14 14:13:25 -0400

    actually finished all stubs

---
+++ Question.java
@@ -0,0 +1,9 @@
+import java.util.AbstractMap;
+import java.util.ArrayList;
+
+
+public class Question {
+    private String prompt;
+    // string is the text displayed to the user, bool is whether the user gets point for choosing that answer
+    private ArrayList<AbstractMap.SimpleEntry<String,Boolean>> answers;
+ }
---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Test {
+    private String name;
+    private ArrayList<Question> questions;
+
+    public Test(String name) {
+        this.name = name;
+    }
+
+    public void setQuestions(ArrayList<Question> questions){
+
+    }
+}
