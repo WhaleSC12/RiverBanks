94c86616a8a3ffa9e9aed5d48e79527e10c7d1f8
DruvP22 <druv@email.sc.edu>
2023-03-14 22:08:39 -0400

    created and almost finished DataLoader and also added the json library to project

---
+++ DataLoader.java
@@ -0,0 +1,42 @@
+/*
+* Data Load loads the users 
+*/
+import java.io.FileReader;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;;
+
+public class DataLoader extends DataConstants{
+	
+	public static ArrayList<RegisteredUser> loadPeople() {
+		ArrayList<RegisteredUser> users = new ArrayList<RegisteredUser>();
+		
+		try {
+			FileReader reader = new FileReader(USERS_FILE_NAME);
+			JSONParser parser = new JSONParser();
+			JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+			
+			for(int i=0; i < peopleJSON.size(); i++) {
+				JSONObject userJSON = (JSONObject)peopleJSON.get(i);
+				String firstName = (String)userJSON.get(USERS_FIRST_NAME);
+				String lastName = (String)userJSON.get(USERS_LAST_NAME);
+                String email = (String)userJSON.get(USERS_EMAIL_STRING);
+                String DOB = (String)userJSON.get(USERS_DOB);// have to change from type String to type Date
+                String phoneNumber = (String)userJSON.get(USERS_PHONE_NUMBER);
+                String userName = (String)userJSON.get(USERS_USER_NAME);
+                String password = (String)userJSON.get(USERS_PASSWORD);	
+                /* got to change DOB type to Date and gotta ask Portia or a TA
+                 * users.add(new RegisteredUser(firstName, lastName, email, DOB, phoneNumber, userName, password));
+                */
+			}
+			
+			return users;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
--- LearningUI.java
+++ LearningUI.java
@@ -1,2 +1,8 @@
+import java.util.Scanner;
+
 public class LearningUI {
+private static final String Welcome = "Welcome to the Learning Management System";
+private String[] mainMenu = {"Login","Create Account"," Search Course", "Create Course", "Logout"};
+private Scanner scanner;
+ 
 }
