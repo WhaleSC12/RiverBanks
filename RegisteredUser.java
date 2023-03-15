23bc0339560ee811d3cd5e7252c0808ebd78e0ed
Raymond Konarski <konarskiraymond@gmail.com>
2023-03-15 08:51:36 -0400

    added proper support for localdate type

--- RegisteredUser.java
+++ RegisteredUser.java
@@ -1,24 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Locale;
 
 public class RegisteredUser {
     protected String firstName;
     protected String lastName;
     protected String email;
-    protected Date dateOfBirth;
+    protected LocalDate dateOfBirth;
     protected String phoneNumber;
     protected String UID;
     protected String username;
     protected String password;
     // <Course, Completed Lessons>
     private ArrayList<AbstractMap.SimpleEntry<Course,Integer>> courses;
+    DateTimeFormatter MDY_formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy", Locale.ENGLISH);
 
-    public RegisteredUser(String firstName, String lastName, String email, Date dateOfBirth, String phoneNumber, String username, String password) {
+    public RegisteredUser(String firstName, String lastName, String email, String dateOfBirth, String phoneNumber, String username, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.dateOfBirth = dateOfBirth;
+        this.dateOfBirth = LocalDate.parse(dateOfBirth, MDY_formatter);
         this.phoneNumber = phoneNumber;
         this.username = username;
         this.password = password;
