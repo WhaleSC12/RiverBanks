2dd5851e19fef8350775abe3c03863fdcd14eaf3
Raymond Konarski <konarskiraymond@gmail.com>
2023-03-16 14:18:02 -0400

    finished datawriter and abstracted writes messily

--- DataWriter.java
+++ DataWriter.java
@@ -6,7 +6,6 @@ import org.json.simple.parser.ParseException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -52,13 +51,6 @@ public class DataWriter {
         return 0;
     }
 
-    /**
-     * Writes the user into users.json to be saved
-     *
-     * @param user the user to be written into storage
-     * @return -1 on failure, otherwise 0
-     * @author Raymond
-     */
     public static int writeUserData(User user) {
 
         // putting all references to RegisteredUser user at top to improve readability
@@ -86,36 +78,21 @@ public class DataWriter {
     }
 
     public static int writeCourseData(Course course) {
-        String courseUUID = course.getUUID().toString();
-        HashMap<String, Object> courseData = new HashMap<>();
+        JSONObject courseData = new JSONObject();
         courseData.put("courseTitle", course.getTitle());
         courseData.put("language", course.getLanguage());
         courseData.put("authorUUID", course.getAuthor().getUUID());
-        ArrayList<HashMap<String, Object>> lessonList = new ArrayList<>();
+        JSONArray lessonList = new JSONArray();
         for (Lesson l : course.getLessons()) {
-            Map<String, Object> tMap = Map.of("title", l.getTitle(), "content", l.getContent(), "test", l.getTest() // TODO: don't write an objet to JSON, unless the lib supports that, still haven't tried
+            Map<String, Object> tMap = Map.of(
+                    "title", l.getTitle(),
+                    "content", l.getContent(),
+                    "test", l.getTest() // TODO: don't write an objet to JSON, unless the lib supports that, still haven't tried
             );
             lessonList.add(new HashMap<>(tMap));
         }
+        courseData.put("lessons", lessonList);
 
-        JSONObject allData;
-        try (FileReader fileReader = new FileReader(COURSES_JSON)) {
-            allData = (JSONObject) new JSONParser().parse(fileReader);
-        } catch (IOException e) {
-            return -2;
-        } catch (ParseException e) {
-            return -3;
-        }
-
-        var old = allData.put(courseUUID, courseData);
-
-        // write back to json
-        try (FileWriter fileWriter = new FileWriter(USERS_JSON)) {
-            fileWriter.write(allData.toJSONString());
-        } catch (IOException e) {
-            return -2;
-        }
-
-        return (old == null) ? 1 : 0;
+        return writeDataAtKey(COURSES_JSON, courseData, course.getUUID());
     }
 }
