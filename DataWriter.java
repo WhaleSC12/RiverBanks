99874893408d84d565794b5361353d7b940e521f
Raymond Konarski <konarskiraymond@gmail.com>
2023-03-17 11:29:51 -0400

    redesigning data writer to work at any depth

--- DataWriter.java
+++ DataWriter.java
@@ -6,6 +6,7 @@ import org.json.simple.parser.ParseException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -15,6 +16,36 @@ public class DataWriter {
     private static final String USER_COURSE_DATA_JSON = "json/dat/userCourseData.json";
     private static final String COURSES_JSON = "json/dat/courses.json";
 
+    private static class JSONWriter {
+        private String filePath;
+        private JSONObject allData;
+        private JSONObject currentJSONObject;
+        private ArrayList<UUID> keyStack = new ArrayList<>();
+
+        protected JSONWriter(String filePath) {
+            this.filePath = filePath;
+        }
+
+        protected JSONWriter obj(UUID key) {
+            currentJSONObject = (JSONObject) currentJSONObject.get(key);
+            keyStack.add(key);
+            return this;
+        }
+
+        protected void write() {
+
+        }
+    }
+
+    /**
+     * Writes data into a JSON file at the given key as a value
+     *
+     * @param targetFile   the JSON within which to write
+     * @param dataToWrite  the JSONObject which should be written into the file at the key
+     * @param keyToWriteAt the key within the target file in which the data should be written
+     * @return -1 upon exception or failure, otherwise 0
+     * @author Raymond Konarski
+     */
     private static int writeDataAtKey(String targetFile, JSONObject dataToWrite, UUID keyToWriteAt) {
         // yes, i realize i'm reading the whole file into memory, but we don't currently have a singleton for all data
         // catching for the sake of doing something with the code later, but otherwise this doesn't do anything but slow everything down
@@ -33,6 +64,16 @@ public class DataWriter {
         return 0;
     }
 
+    /**
+     * Writes data into a JSON file at the given key within another JSON object at the keyToWriteWithin as a value
+     *
+     * @param targetFile       the JSON within which to write
+     * @param dataToWrite      the JSONObject which should be written into the file at the key
+     * @param keyToWriteAt     the key within the target file in which the data should be written
+     * @param keyToWriteWithin the key within which the object which the keyToWriteAt points to reside
+     * @return -1 upon exception or failure, otherwise 0
+     * @author Raymond Konarski
+     */
     private static int writeDataWithinKeyAtKey(String targetFile, JSONObject dataToWrite, UUID keyToWriteAt, UUID keyToWriteWithin) {
         JSONObject allData;
         try (FileReader fileReader = new FileReader(targetFile)) {
@@ -51,6 +92,12 @@ public class DataWriter {
         return 0;
     }
 
+    /**
+     * Save the user's data (not course progress and grades) into the User JSON
+     *
+     * @param user user whose data should be saved
+     * @return -1 on failure, 0 otherwise
+     */
     public static int writeUserData(User user) {
 
         // putting all references to RegisteredUser user at top to improve readability
@@ -67,6 +114,13 @@ public class DataWriter {
         return writeDataAtKey(USERS_JSON, tempData, userUUID);
     }
 
+    /**
+     * Store's a user's progress and grades within a course
+     *
+     * @param userUUID user whose course progress is being written
+     * @param course   course whose data is being saved
+     * @return -1 on failure, else 0
+     */
     public static int writeUserCourseData(UUID userUUID, Course course) {
         JSONObject userCourses = new JSONObject();
         JSONObject courseData = new JSONObject();
@@ -77,6 +131,12 @@ public class DataWriter {
         return writeDataWithinKeyAtKey(USER_COURSE_DATA_JSON, courseData, course.getUUID(), userUUID);
     }
 
+    /**
+     * Store's course information, such as lessons, content, tests, etc. into the courses json
+     *
+     * @param course course whose data is being stored
+     * @return -1 on failure, else 0
+     */
     public static int writeCourseData(Course course) {
         JSONObject courseData = new JSONObject();
         courseData.put("courseTitle", course.getTitle());
