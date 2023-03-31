d30e7c2fc7fb24a083ea0fa11638ca1e4b6c9f3f
Raymond Konarski <konarskiraymond@gmail.com>
2023-03-30 23:33:37 -0400

    quick code cleanup

--- Facade.java
+++ Facade.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 
 //Where all the method will go that will be used in the LearningUI 
 public class Facade {
-    private static  User currentUser;
+    private static User currentUser;
     private static final UserData userData = UserData.getInstance();
     private Course course;
     private Module module;
@@ -21,7 +21,7 @@ public class Facade {
      * @return
      */
 
-    public static  User Login(String username, String password) {
+    public static User Login(String username, String password) {
         for (User u : userData.userList) {
             if (u.getUsername().equals(username) && u.getPassword().equals(password)) {
                 return userData.getUser(username);
@@ -56,23 +56,25 @@ public class Facade {
     }
 
 
-/** void method that allows Teacher to create Lesson 
-* asking user to enter name,description,lanuage and amount of lessons for the course and then
-* asks the user how many lessons they want and if they want to add a Test to it and write the test with 
-* the questions and answer choices
-* @param course       basic course info and holds all lessons within modules
-* @param module       sub-topics within lessons which contain the actual informative content
-* @param Lesson       lessons are groups of modules centered around some topic
-* @param test         basic test info such as name and description and holds all questions
-* @param question     basic question info such as question and answers choices and holds correct choice
-*/public static Course creatCourse(String title, String description, UUID authorUUID, Language language)
-{ 
-    Scanner scanner = new Scanner(System.in); 
+    /**
+     * void method that allows Teacher to create Lesson
+     * asking user to enter name,description,lanuage and amount of lessons for the course and then
+     * asks the user how many lessons they want and if they want to add a Test to it and write the test with
+     * the questions and answer choices
+     *
+     * @param course   basic course info and holds all lessons within modules
+     * @param module   sub-topics within lessons which contain the actual informative content
+     * @param Lesson   lessons are groups of modules centered around some topic
+     * @param test     basic test info such as name and description and holds all questions
+     * @param question basic question info such as question and answers choices and holds correct choice
+     */
+    public static Course creatCourse(String title, String description, UUID authorUUID, Language language) {
+        Scanner scanner = new Scanner(System.in);
 
-   Course course = new Course(title, description, authorUUID, language);
-    
-   System.out.print("Enter the number of modules: ");
-   int numModules = Integer.parseInt(scanner.nextLine());
+        Course course = new Course(title, description, authorUUID, language);
+
+        System.out.print("Enter the number of modules: ");
+        int numModules = Integer.parseInt(scanner.nextLine());
 
         ArrayList<Module> modules = new ArrayList<>();
         for (int i = 0; i < numModules; i++) {
--- LearningUI.java
+++ LearningUI.java
@@ -3,20 +3,22 @@ import java.util.UUID;
 
 
 public class LearningUI {
-public static void main(String[] args) {
- LearningUI runPlease = new LearningUI();
- runPlease.Intro();
-}
-private static final String Welcome = "Welcome to the Learning Management System";
-private String[] mainMenu = {"Login","Create Account","Search Course", "Create Course", "Logout"};
-private Scanner scanner;
-Facade facade = new Facade();
-private static User currentUser;
-public void Intro() {
-    scanner = new Scanner(System.in); 
-    System.out.println(Welcome);
-    while(true) {
-    displayMainMenu();
+    public static void main(String[] args) {
+        LearningUI runPlease = new LearningUI();
+        runPlease.Intro();
+    }
+
+    private static final String Welcome = "Welcome to the Learning Management System";
+    private final String[] mainMenu = {"Login", "Create Account", "Search Course", "Create Course", "Logout"};
+    private Scanner scanner;
+    Facade facade = new Facade();
+    private static User currentUser;
+
+    public void Intro() {
+        scanner = new Scanner(System.in);
+        System.out.println(Welcome);
+        while (true) {
+            displayMainMenu();
 
             int userChoice = getUserChoice(mainMenu.length);
 
@@ -27,22 +29,11 @@ public void Intro() {
             if (userChoice == mainMenu.length - 1) break;
 
             switch (userChoice) {
-                case (0):
-                    loginMethod();
-                    break;
-                case (1):
-                    createAccount();
-                    break;
-                case (2):
-                    existingCourse();
-                    break;
-                case (3):
-                    createCourse();
-                    break;
-                case (4):
-                    logout();
-                    break;
-
+                case (0) -> loginMethod();
+                case (1) -> createAccount();
+                case (2) -> existingCourse();
+                case (3) -> createCourse();
+                case (4) -> logout();
             }
         }
     }
@@ -54,33 +45,33 @@ public void Intro() {
      * @return null
      */
     public void loginMethod() {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter your username");
-    System.out.println("Enter q to cancel");
-   String usernameinput = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your username");
+        System.out.println("Enter q to cancel");
+        String usernameinput = scanner.nextLine();
 
 
-   while(true){ 
-   if(usernameinput == "q") {
-    displayMainMenu();
-    } else {
-    System.out.println("Enter your password");
-    String passwordinput = scanner.nextLine();   
-    currentUser = Facade.Login(usernameinput, passwordinput);
-    UUID userUUID = currentUser.getUUID();
+        while (true) {
+            if (usernameinput == "q") {
+                displayMainMenu();
+            } else {
+                System.out.println("Enter your password");
+                String passwordinput = scanner.nextLine();
+                currentUser = Facade.Login(usernameinput, passwordinput);
+                UUID userUUID = currentUser.getUUID();
 
-    if(usernameinput != null || passwordinput != null) {
-    displayMainMenu();
-    } else {
-        continue;
-    }
-    }
-    }
-    
+                if (usernameinput != null || passwordinput != null) {
+                    displayMainMenu();
+                } else {
+                    continue;
+                }
+            }
+        }
 
-    /*call facade after getting the login */
-    /*set current user to you */
-    /*when they log out set current user to null */
+
+        /*call facade after getting the login */
+        /*set current user to you */
+        /*when they log out set current user to null */
     }
 
     /*
@@ -88,80 +79,79 @@ public void Intro() {
      * username, password, first name, last name, email and phone number
      */
     private void createAccount() {
-    String newusernameinput;
-    String newpasswordinput;
-    String firstnameinput;
-    String lastnameinput;
-    String emailinput;
-    String phonenumberinput;
-    String clearanceinput;
+        String newusernameinput;
+        String newpasswordinput;
+        String firstnameinput;
+        String lastnameinput;
+        String emailinput;
+        String phonenumberinput;
+        String clearanceinput;
 
-    while(true){
-    System.out.println("Create a username");
-    newusernameinput = scanner.nextLine();
-    System.out.println("Enter q to cancel");
-    if (newusernameinput == "q") {
-        displayMainMenu();
+        while (true) {
+            System.out.println("Create a username");
+            newusernameinput = scanner.nextLine();
+            System.out.println("Enter q to cancel");
+            if (newusernameinput.equals("q")) {
+                displayMainMenu();
+            }
+            System.out.println("Create a password");
+            newpasswordinput = scanner.nextLine();
+            UserData userName = UserData.getInstance();
+            if (userName.getUser(newusernameinput) != null) {
+                System.out.println("Username already taken");
+                continue;
+            }
+            break;
+        }
+
+        while (true) {
+            System.out.println("Enter your first name");
+            firstnameinput = scanner.nextLine();
+            System.out.println("Enter your last name");
+            lastnameinput = scanner.nextLine();
+            System.out.println("Enter your email address");
+            emailinput = scanner.nextLine();
+            System.out.println("Enter your phone number");
+            phonenumberinput = scanner.nextLine();
+            System.out.println("Are you a teacher or a student? Enter student for student, Enter teacher for teacher");
+            clearanceinput = scanner.nextLine();
+            if (clearanceinput.equals("student")) {
+                String userClearance = "Student";
+            } else if (clearanceinput.equals("teacher")) {
+                String userClearance = "Teacher";
+            } else {
+                System.out.println("Invalid input");
+                continue;
+            }
+            break;
+        }
+
     }
-    System.out.println("Create a password");
-    newpasswordinput = scanner.nextLine();
-    UserData userName = UserData.getInstance();
-    if (userName.getUser(newusernameinput) != null){
-        System.out.println("Username already taken");
-        continue;
-    }
-    break;
-    }
-    
-    while(true){
-    System.out.println("Enter your first name");
-    firstnameinput = scanner.nextLine();
-    System.out.println("Enter your last name");
-    lastnameinput = scanner.nextLine();
-    System.out.println("Enter your email address");
-    emailinput = scanner.nextLine();
-    System.out.println("Enter your phone number");
-    phonenumberinput = scanner.nextLine();
-    System.out.println("Are you a teacher or a student? Enter student for student, Enter teacher for teacher");
-    clearanceinput = scanner.nextLine();
-    if (clearanceinput.equals("student")){
-    String userClearance = "Student";
-    } else if (clearanceinput.equals("teacher")){
-    String userClearance = "Teacher";
-    } else {
-    System.out.println("Invalid input");
-    continue;
-    }
-    break;
-    }
-    
-    }
+
     private void createCourse() {
-    UUID uniqueIdentifier = UUID.randomUUID();
-    
-    String title = "Beginner Java lesson";
-    String description = "A beginner's guide to early java fundamentals";
-    facade.creatCourse(title, description, uniqueIdentifier, Language.Java);
-    System.out.println("Enter 1 to add to an existing course. Enter 2 to create a new course.");
+        UUID uniqueIdentifier = UUID.randomUUID();
+
+        String title = "Beginner Java lesson";
+        String description = "A beginner's guide to early java fundamentals";
+        Facade.creatCourse(title, description, uniqueIdentifier, Language.Java);
+        System.out.println("Enter 1 to add to an existing course. Enter 2 to create a new course.");
     }
 
     private void existingCourse() {
-                System.out.println("Choose a course");
+        System.out.println("Choose a course");
     }
 
 
     private void logout() {
         displayMainMenu();
         /*return to main menu */
-        
-    }
-    
 
+    }
 
 
     private int getUserChoice(int numCommands) {
         System.out.print("Enter a number: ");
-    
+
         String input = scanner.nextLine();
         int command = Integer.parseInt(input) - 1;
 
